b321be180de585664afa8972810b1fd67bc35bbc
Jeffrey Azevedo <42226678+jeffreyazevedo@users.noreply.github.com>
2023-06-01 16:16:39 -0600

    PMGR-10818 Switch to using 'sf' tarball and 'sf' commands (#4)
    * PMGR-10818 Switch to using the 'sf' tarball and sf auth command
    
    * PMGR-10818 Missed two sfdx commands

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ try {
 }
 
 function installSFDX(){
-  var tarballBase = 'https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable'
-  var tarballFile = 'sfdx-linux-x64.tar.xz'
+  var tarballBase = 'https://developer.salesforce.com/media/salesforce-cli/sf/channels/stable'
+  var tarballFile = 'sf-linux-x64.tar.xz'
 
   var tarballOverride = core.getInput('tarball-url')
   if(tarballOverride ){
@@ -22,7 +22,7 @@ function installSFDX(){
   var createDir = 'mkdir sfdx'
   var unzip = 'tar xJf /tmp/' + tarballFile + ' -C sfdx --strip-components 1'
   var install = 'echo "`pwd`/sfdx/bin" >> $GITHUB_PATH'
-  var version = 'sfdx/bin/sfdx --version && sfdx/bin/sfdx plugins --core'
+  var version = 'sfdx/bin/sf --version && sfdx/bin/sf plugins --core'
   exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+version, function(error, stdout, stderr){
     if(error) throw(stderr)
     core.info(stdout)
@@ -37,7 +37,7 @@ function createAuthFile(){
 
 function authSFDX(){
   var params = '-d -s -a SFDX-ENV'
-  exec('sfdx/bin/sfdx auth:sfdxurl:store -f /tmp/sfdx_auth.txt '+params, function(error, stdout, stderr){
+  exec('sfdx/bin/sf org login sfdx-url -f /tmp/sfdx_auth.txt '+params, function(error, stdout, stderr){
     if(error) throw(stderr)
 	core.info(stdout)
   })
